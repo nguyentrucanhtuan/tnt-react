43c125c4ae29f65ad8ee68f0bc4a1b8f0cf3b967
Lam Huynh <tlamhuynh@gmail.com>
2016-12-07 20:22:08 +0700

    Product Card

--- app/pages/category/ProductCard.js
+++ app/pages/category/ProductCard.js
@@ -1,7 +1,7 @@
 import React, {Component, PropTypes} from 'react';
 import {connect} from 'react-redux';
 //import Image from './../../components/Image'
-import Image as ImageComponent from 'ainojs-react-image';
+import {ImageComponent as Image} from 'ainojs-react-image';
 
 import {ListItem, Icon} from 'react-onsenui';
 import { getRoute } from './../../routes'
